aa7d984097576c79520c62079016ece6b8958348
Nicholas Edison <nicholasedison00@gmail.com>
2023-12-07 21:23:00 +0700

    Membuat layout Profile Screen

--- frontend/lib/screen/profile_screen.dart
+++ frontend/lib/screen/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ukm_project/widgets/custom_navigation_bar.dart';
+import 'package:ukm_project/widgets/profile_info_item.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
@@ -13,7 +14,80 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: const Placeholder(),
+      backgroundColor: Color(0xfff2f8fe),
+      body: const SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            SizedBox(height: 20),
+            CircleAvatar(
+              backgroundColor: Colors.pinkAccent,
+              radius: 50,
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Text(
+                    'Informasi Akun',
+                    style: TextStyle(
+                      color: Colors.orangeAccent,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ProfileInfoItem(
+                        icon: Icons.account_circle_outlined,
+                        value: 'Mbak IU',
+                        color: Colors.blueAccent
+                    ),
+                  SizedBox(height: 10),
+                  ProfileInfoItem(
+                      icon: Icons.mail_outline_outlined,
+                      value: 'MbakIU@gmail.com',
+                      color: Colors.blueAccent
+                  ),
+                  SizedBox(height: 10),
+                  ProfileInfoItem(
+                      icon: Icons.phone_outlined,
+                      value: '+628xxxxxxxxxx',
+                      color: Colors.blueAccent
+                  ),
+                  SizedBox(height: 10),
+                  ProfileInfoItem(
+                      icon: Icons.lock_outlined,
+                      value: '****',
+                      color: Colors.blueAccent
+                  ),
+                  SizedBox(height: 20),
+                  Text(
+                    'Detail Informasi',
+                    style: TextStyle(
+                        color: Colors.orangeAccent,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  ProfileInfoItem(
+                      icon: Icons.logout_outlined,
+                      value: 'Keluar',
+                      color: Colors.blueAccent
+                  ),
+                  SizedBox(height: 10),
+                  ProfileInfoItem(
+                      icon: Icons.wallet_outlined,
+                      value: 'WanderPay',
+                      color: Colors.blueAccent
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
       bottomNavigationBar: CustomNavigationBar(
         onItemSelected: (index) {
           _selectedIndex = index;
