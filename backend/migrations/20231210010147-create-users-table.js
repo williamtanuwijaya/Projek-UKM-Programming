fbb1a5a91d6a002500e4b027ffca94eac74d0c3c
williamtanuwijaya <will.tw0807@gmail.com>
2023-12-10 17:48:24 +0700

    make table users

--- backend/migrations/20231210010147-create-users-table.js
+++ backend/migrations/20231210010147-create-users-table.js
@@ -2,21 +2,11 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', { id: Sequelize.INTEGER });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  },
 };
